81015e90319b5c5da68339dfc1a43e89b78484c7
ramonmoreno1 <ramonmoreno1@hotmail.es>
2024-09-01 19:14:03 +0200

    mvc ecmascript modules repasar import json

---
+++ 04-mvc/schemes/schemaValiMovie.js
@@ -0,0 +1,37 @@
+// eesquema de validaciones
+import z from 'zod'
+
+const movieSchema = z.object({
+    title: z.string({
+        invalid_type_error: 'Movie title must be a string',
+        required_error: 'Movie title is requires'
+    }),
+    year: z.number().int().min(1900).max(2024),
+    director: z.string({
+        invalid_type_error: 'Director must be a string'
+    }),
+    duration: z.number().int().positive().max(300),
+    poster: z.string().url({
+        message: 'Poster must be a valid URL'
+    }),
+    rate: z.number().min(0).max(10).default(5),
+    genre: z.array(z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']))
+
+})
+
+function validateMovie(object) {
+    return movieSchema.safeParse(object)
+    // safeParse devuelve un object result err o data
+}
+
+// validación parcial de datos
+
+function validatePartialMovie(object) {
+    // partial() muy usado en typeScript hace que las props sean opcionales
+    return movieSchema.partial().safeParse(object)
+}
+
+export {
+    validateMovie,
+    validatePartialMovie
+}
